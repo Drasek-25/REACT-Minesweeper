373773cd6120f5439480801f0629016acd3daa85
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2021-03-15 18:34:18 -0700

    added a temporary WIP header to game in preparation for live build

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GameWindow from "./components/GameWindow";
 function App() {
    return (
       <div className="App">
+         <h3>This game is still a work in progress.</h3>
          <GameWindow />
       </div>
    );
